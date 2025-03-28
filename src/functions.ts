e17655d46f82c45c9669d8dae32dd92c990cb1f6
y_inoue <ymir.3479@gmail.com>
2025-03-28 22:36:13 +0900

    性別を記録 前回申込公演記録

--- src/functions.ts
+++ src/functions.ts
@@ -13,6 +13,12 @@ export function getFormValSet(storageKey: string, tag: string) {
       const inputElement = element as HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
       params[inputElement.name] = inputElement.value
     })
+    active_form.querySelectorAll('input[type=radio]').forEach((element) => {
+      const radioElement = element as HTMLInputElement
+      if (radioElement.checked) {
+        params[radioElement.name] = radioElement.value
+      }
+    })
   }
 
   delete params['utf8']
@@ -29,13 +35,57 @@ export function getFormValSet(storageKey: string, tag: string) {
  * @param storageKey ストレージキー
  */
 export function setFormVal(storageKey: string): void {
+  console.log('読み込みが完了しました。')
   const storageValue = localStorage.getItem(storageKey) || '{}'
   const params = JSON.parse(storageValue) as { [key: string]: string }
 
   for (const key in params) {
+    const radioElems = document.querySelectorAll(
+      `input[type="radio"][name='${key}']`,
+    ) as NodeListOf<HTMLInputElement>
+    if (radioElems.length > 0) {
+      radioElems.forEach((radio) => {
+        radio.checked = radio.value === params[key]
+      })
+      continue
+    }
     const inputElem = document.querySelector(`[name='${key}']`) as HTMLInputElement
     if (inputElem && inputElem.type !== 'hidden') {
       inputElem.value = params[key]
     }
   }
+}
+
+/**
+ * 第1希望の選択肢のラベルを localStorage に保存します
+ */
+export function saveAutoSelectedLabel(storageKey: string): void {
+  const radioButtons = document.querySelectorAll<HTMLInputElement>(
+    'input[type="radio"][name="hope_event_perf_cd"]',
+  )
+
+  radioButtons.forEach((radio) => {
+    radio.addEventListener('change', (event) => {
+      const selectedRadio = event.target as HTMLInputElement
+      const parentParagraph = selectedRadio.closest('p')
+
+      if (parentParagraph) {
+        const label = parentParagraph.textContent?.trim() || ''
+        console.log(label)
+        localStorage.setItem(storageKey, label)
+        const spanElement = document.querySelector<HTMLSpanElement>('#before-selected')
+        if (spanElement) {
+          spanElement.textContent = label
+        }
+      }
+    })
+  })
+}
+
+/**
+ * 第1希望の選択肢のラベルを localStorage から取得します
+ */
+export function getSavedLabel(storageKey: string): string | null {
+  const savedLabel = localStorage.getItem(storageKey)
+  return savedLabel
 }
